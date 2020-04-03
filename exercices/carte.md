07afdeca91bfc40e13f07d8b8363e2c72b7e7941
Soraya <soraya.meystre@heig-vd.ch>
2020-04-03 13:22:43 +0200

    Add the map exercise

---
+++ exercices/carte.md
@@ -0,0 +1,7 @@
+# Dessiner une carte
+
+## Avec D3
+
+> Utilisez les données et la librairie de votre choix. Vous pouvez faire  cet exercice dans votre repo de cours ou dans un autre repo. Ajoutez  simplement un lien vers le code dans le fichier `exercices/carte.md`.C'est la façon dont sont représentées les coordonnées, par un point, une ligne, une surface, par exemple.
+
+Lien pour visualiser la carte : 
---
+++ exercices/carte/src/carte.js
@@ -0,0 +1,69 @@
+import * as d3 from 'd3';
+import suisse from '../suisse.json';
+
+let pathCreator = d3.geoPath();
+let countryPaths = suisse.features.map(pathCreator);
+
+const width = 1000;
+const xMin = 2486148
+const yMin = 1076497
+const xMax = 2833837
+const yMax = 1295164
+const WIDTH = 1000
+const HEIGHT = width * ((yMax - yMin) / (xMax - xMin))
+
+const projection = d3.geoTransform({
+  point: function(x, y) {
+    this.stream.point(
+      (x - xMin) / (xMax - xMin) * WIDTH,
+      HEIGHT - (y - yMin) / (yMax - yMin) * HEIGHT
+    )
+  }
+});
+pathCreator = d3.geoPath().projection(projection)
+
+const svg = d3.select('body')
+  .append('svg')
+  .attr('width', WIDTH)
+  .attr('height', HEIGHT)
+
+svg.selectAll('path')
+  .data(suisse.features)
+  .enter()
+  .append('path')
+  .attr('d', pathCreator)
+  .attr('stroke', 'SandyBrown')
+  .attr('fill', 'PapayaWhip')
+
+
+
+const projectX = x => (x - xMin) / (xMax - xMin) * WIDTH
+const projectY = y => HEIGHT - (y - yMin) / (yMax - yMin) * HEIGHT
+
+const projectionPoint = d3.geoTransform({
+  point: function(x, y) {
+    this.stream.point(projectX(x), projectY(y)) // utiliser les fonctions
+  }
+})
+const pathCreatorPoint = d3.geoPath().projection(projectionPoint)
+
+
+svg.selectAll('path')
+  .data(suisse.features)
+  .enter()
+  .append('path')
+  .attr('d', pathCreatorPoint)
+
+// les coordonnées de la gare d'Yverdon
+const mont = [2533555,1149818]
+
+// projeter le point
+const montX = projectX(mont[0])
+const montY = projectY(mont[1])
+
+// dessiner le point
+svg.append('circle')
+  .attr('cx', montX)
+  .attr('cy', montY)
+  .attr('r', 5)
+  .attr('fill', 'SandyBrown')
