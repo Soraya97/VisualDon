2e3a44937d219ee9863e81352e96e188f0391e6c
Soraya <soraya.meystre@heig-vd.ch>
2020-04-24 08:24:32 +0200

    index.js and readme updated

--- projet/readme.md
+++ projet/readme.md
@@ -5,10 +5,40 @@
 
 ## Jeu de données
 
-Nombre d'infectés, de morts et de guéris du Covid-19 par continent et dans le monde : 
+Nombre d'infectés, de morts et de guéris du Covid-19 par continent et dans le monde :
 https://docs.google.com/spreadsheets/d/1tjN4GNTzRV6nYWHu8xSLUM-rmyEwqrqpmpyOb6Je45I/edit#gid=717155371.
 
 ## Idée
 
 L'idée est de représenter les courbes des infectés, des morts et des guéris par continents et selon une timeline interactive (dans ce genre-là: http://heig-datavis2020.surge.sh/20200327/rosling-d3/).
 
+## Liens utiles
+
+https://github.com/d3/d3-shape#areas
+
+Interaction avec des bulles
+https://github.com/idris-maps/heig-datavis-2020/tree/master/modules/rosling/graphique_d3
+
+Types de visualisation de données
+https://observablehq.com/@idris-maps/visualisation-de-donnees?collection=@idris-maps/heig-visdom-2020
+
+Galerie de graphes avec d3
+https://www.d3-graph-gallery.com/
+
+Graphe de densité avec d3
+https://www.d3-graph-gallery.com/graph/density_basic.html
+
+Affichage interactif
+https://www.datavis.fr/index.php?page=transition
+
+Dessiner une courbe
+https://www.datavis.fr/index.php?page=linearchart
+
+Dessiner deux courbes
+https://www.datavis.fr/index.php?page=twolinearchart
+
+Ajouter des dates sur les axes
+http://bl.ocks.org/phoebebright/3059392
+
+d3-line
+https://observablehq.com/@d3/d3-line
--- projet/src/index.js
+++ projet/src/index.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3';
 import data from '../data/dataCovid19.json';
 
-const WIDTH = 1000
+const WIDTH = 1500
 const HEIGHT = 500
 const MARGIN = 5
 
@@ -12,6 +12,7 @@ const svg = d3.select('.covid')
 
 const MARGIN_LEFT = 100
 const MARGIN_BOTTOM = 50
+
 
 //** AXIS **//
 /* Y axis */
@@ -24,28 +25,28 @@ const yAxis = d3.axisLeft(yScale)
   .tickFormat(d => `${d/1000}k`)
 
 const lAxis = svg.append('g')
+  .attr("class", "yaxis")
   .attr('transform', `translate(${MARGIN_LEFT  * 0.7}, 0)`)
   .call(yAxis)
 
 svg.append("text")
   .attr("text-anchor", "start")
   .attr('transform', `rotate(-90, ${WIDTH / MARGIN - MARGIN_LEFT}, ${HEIGHT})`)
-  .attr("x", HEIGHT / 5 + MARGIN_BOTTOM)
-  .attr("y", HEIGHT - 80)
+  .attr("x", HEIGHT / 5 + MARGIN_BOTTOM + 90)
+  .attr("y", HEIGHT - MARGIN_LEFT - 70)
   .text("Nombre de victimes du Covid-19");
 
 /* X axis */
-var mindate = new Date(2019, 12, 16),
-  maxdate = new Date(2020, 4, 20);
-const parseTime = d3.timeParse("%d/%m/%Y");
+const parseTime = d3.timeParse("%Y-%m-%d")
+const mindate = d3.min(data, d => parseTime(d.date)),
+  maxdate = d3.max(data, d => parseTime(d.date));
 let date = parseTime(data.date)
 const xScale = d3.scaleTime()
   .domain([mindate, maxdate])
-  .range([WIDTH, 0])
+  .range([0, WIDTH])
 
 const xAxis = d3.axisBottom(xScale)
-  .ticks(25)
-// .tickFormat(d => `${d/1000}k`)
+  .ticks(10)
 
 const bAxis = svg.append('g')
   .attr("class", "xaxis")
@@ -59,8 +60,42 @@ svg.append("text")
   .text("Date")
 
 
+//** affichage des courbes **//
+
+// const batons = svg.append('g')
+//   .attr('transform', `translate(${MARGIN_LEFT}, 0)`)
+//
+// batons.selectAll('rect')
+//   .data(data)
+//   .enter()
+//   .append('rect')
+//   .attr('x', (d, i) =>  i * ((WIDTH - MARGIN_LEFT) / data.length))
+//   .attr('width', ((WIDTH - MARGIN_LEFT) / data.length) - MARGIN)
+//   .attr('y', d => yScale(d.Monde.morts))
+//   .attr('height', d => HEIGHT - MARGIN_BOTTOM - yScale(d.Monde.morts))
+//   .attr('fill', 'steelblue')
+
+const line = d3.line()
+  .x(d => xScale(d.date))
+  .y(d => yScale(d.Monde.morts));
+
+svg.selectAll('yaxis')
+  .data(data)
+  // .data(yScale.ticks(10))
+  .enter()
+  .append("line")
+  .attr("x1", 0)
+  .attr("x2", WIDTH)
+  .attr("y1", d => yScale(d))
+  .attr("y2", d => yScale(d))
+
+svg.append("path")
+  .data(data)
+  .attr("d", line);
+
 
 //** Couleur selon région **//
+
 // export const getColorByRegion = ({
 //   region
 // }) => {
@@ -79,5 +114,3 @@ svg.append("text")
 //       return '#ffd92f'
 //   }
 // }
-
-//** affichage des courbes **//
